e034cb349d1736c9789c2e5c98ab5ffe038cd4eb
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-27 13:43:20 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,20 @@ def prepare_dataset(csv_file):
   dataset = dataset.reset_index(drop=True)
   print("Shape of dataset from {} file: ".format(csv_file), dataset.shape)
   return dataset
+
+
+def extract_diff_common(dataset_1, dataset_2):
+  col = dataset_1.columns[0]
+  common = list(
+                set(dataset_1['{}'.format(col)].values) & set(dataset_2['{}'.format(col)].values)
+                )
+  extra_1 = list(
+                set(dataset_1['{}'.format(col)].values) - set(dataset_2['{}'.format(col)].values)
+                )
+  extra_2 = list(
+              set(dataset_2['{}'.format(col)].values) - set(dataset_1['{}'.format(col)].values)
+                )
+  print("Common images: ", len(common), 
+        "\nDifferent images(dataset_1 only): ", len(extra_1),
+        "\nDifferent images(dataset_2 only): ", len(extra_2))
+  return common, extra_1, extra_2  
